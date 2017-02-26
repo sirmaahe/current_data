1402a76e654cca25264f59900af62e7dff7d99e1
turbo_jesus <sirmaahe@gmail.com>
2017-02-26 21:48:47 +0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,21 @@
+import requests
+
+from app import app
+from config import YANDEX_CLOUD_API_KEY
+
+
+def get_speech_file_from_text(text):
+    url = 'https://tts.voicetech.yandex.net/generate'
+    params = {
+        'text': text,
+        'format': 'wav',
+        'lang': 'ru‑RU',
+        'speaker': 'zahar',
+        'key': YANDEX_CLOUD_API_KEY,
+    }
+    response = requests.get(url, params)
+    with open(app.root_path + '/date.wav', 'wb') as f:
+        for chunk in response:
+            f.write(chunk)
+
+    return
